e8875f27b6ba1a9ef031922b8176584fdf648557
Luis Ricardo Alves Santos <luisricardo2825@gmail.com>
2023-09-11 02:46:29 -0300

    Correção script para build via cli

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 const { build } = require("./main.js");
-const args = process.argv.slice(2);
 
-const regex = /--build(\=)?([\w\W]*)?/;
+function buildCommand() {
+  const args = process.argv.slice(2);
 
-const exec = args.find((item) => regex.test(item));
-console.log(args);
-if (exec) {
-  const result = regex.exec(exec);
-  let params = null;
-  if (result) {
-    if (result[2] && result[2].includes(",")) {
-      params = result[2].split(",");
-    } else {
-      params = result[2];
+  const regex = /--build(\=)?([\w\W]*)?/;
+
+  const exec = args.find((item) => regex.test(item));
+  if (exec) {
+    const result = regex.exec(exec);
+    let params = null;
+    if (result) {
+      if (result[2] && result[2].includes(",")) {
+        params = result[2].split(",");
+      } else {
+        params = result[2];
+      }
     }
+    if (!Array.isArray(params)) return build(params);
+    params.forEach((o) => {
+      build(o);
+    });
   }
-  if (!Array.isArray(params)) {
-    build(params);
-  }
-  params.forEach((o) => {
-    build(o);
-  });
 }
 module.exports.build = build;
