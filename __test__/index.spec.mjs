b1817c43d0bd413f17dfa78d22d8a83c68aedbf7
Luis Ricardo Alves Santos <luisricardo2825@gmail.com>
2023-09-24 11:31:36 -0300

    Correção teste

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -1,10 +1,25 @@
 import test from "ava";
 
-test("Build", async (t) => {
+test("Build in code", async (t) => {
   const { build } = (await import("../index.js")).default;
 
   t.is(
-    build(),
+    build({
+      jsp: [],
+      outDir: "dist",
+      src: "./build",
+    }),
     true
   );
+});
+
+test("Build", async (t) => {
+  let ok = true;
+  try {
+    const { build } = await import("../index.js");
+    ok = build();
+  } catch (e) {
+    ok = false;
+  }
+  t.is(ok, true);
 });
