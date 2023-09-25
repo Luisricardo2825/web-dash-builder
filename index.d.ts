6a0ec78bed0de544ef861945a52de24458ce019d
Luis Ricardo Alves Santos <luisricardo2825@gmail.com>
2023-09-25 09:49:34 -0300

    Correção tipagem

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 import type { ConfigSchema } from "./main";
 import type { Plugin } from "vite";
-export function plugin(options: {
+export function plugin(options?: {
   devConfig?: ConfigSchema;
   prodConfig?: ConfigSchema;
 }): Plugin[];
