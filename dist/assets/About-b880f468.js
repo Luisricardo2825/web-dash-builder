9cce3b161d321f35cf3e9d1d6b64b984ad947301
Luis Ricardo Alves Santos <luisricardo2825@gmail.com>
2025-01-30 13:51:57 -0300

    fix #1: Correção de conexões adicionais sendo abertas ao consultar o dialeto do banco de dados

---
+++ dist/assets/About-b880f468.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-1947ef3d.js";import{L as i}from"./index-f20aadc4.js";import{u as t}from"./App-647b3911.js";function a(){const s=t();return o.jsxs("div",{children:[s.pathname,window.__routes.map(r=>o.jsxs("div",{children:[o.jsx(i,{to:r,children:r}),o.jsx("br",{})]},r))]})}export{a as default};
---
+++ dist/assets/index-499f9bb6.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-1947ef3d.js";import{L as i}from"./index-f20aadc4.js";import{u as n}from"./App-647b3911.js";function a(){const s=n();return o.jsxs("div",{children:[s.pathname,window.__routes.map(r=>o.jsxs("div",{children:[o.jsx(i,{to:r,children:r}),o.jsx("br",{})]},r))]})}export{a as default};
